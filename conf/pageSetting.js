3f264ee6d2217362c4168b7baf5db47c54ff9d36
Liu-Kang <1886882350@163.com>
2016-11-19 19:06:33 +0800

    complete record show

--- conf/pageSetting.js
+++ conf/pageSetting.js
@@ -8,7 +8,7 @@
 module.exports = {
 	//定义动画种类
 	animate:[
-		'typewriter','fade','scale','fadeScale','slideLeft',
+		'typewriter','fade','scale','slideLeft',
 		'slideRight','slideTop','slideBottom','rotate',
 		'rotateLeft','rotateRight'
 	],
@@ -16,11 +16,11 @@ module.exports = {
 	staticBg:['bg1','bg2','bg3','bg4','bg5','bg6','bg7'],
 	dynamicBg:['neon','rose','snow','star'],
 	//设定背景音乐
-	music:[
-		'miss you finally.mp3',
-		'valentine.mp3',
-		'when you say nothing at all.mp3',
-		'遇见.mp3',
-		'专属天使.mp3'
-	]
+	music:{
+		'爱如空气.mp3':'http://antiserver.kuwo.cn/anti.s?format=aac|mp3&rid=MUSIC_234728&type=convert_url&response=res',
+		'valentine.mp3':'http://antiserver.kuwo.cn/anti.s?format=aac|mp3&rid=MUSIC_2120748&type=convert_url&response=res',
+		'when you say nothing at all.mp3':'http://antiserver.kuwo.cn/anti.s?format=aac|mp3&rid=MUSIC_472413&type=convert_url&response=res',
+		'遇见.mp3':'http://antiserver.kuwo.cn/anti.s?format=aac|mp3&rid=MUSIC_203307&type=convert_url&response=res',
+		'小酒窝.mp3':'http://antiserver.kuwo.cn/anti.s?format=aac|mp3&rid=MUSIC_295835&type=convert_url&response=res'
+	}
 }
